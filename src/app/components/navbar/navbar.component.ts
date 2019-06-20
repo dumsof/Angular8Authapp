987ff90e32b76d3c8fabd8c914164b3c2ef81066
darwinum <dun34@hotmail.com>
2019-06-20 10:35:09 -0500

    se cambia las comillas.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { AuthService } from "../../services/auth.service";
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styles: []
+  templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
 
